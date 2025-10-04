3c74e26f4e9881124bb3786f4d6ec93ec85944c1
luyuan <luyuan202509@gmail.com>
2025-10-04 16:37:36 +0800

    使用<|unk|> 替换没在在字典中的词元

--- ch02/01_main_chapter_code/SimpleTokenizerV2.py
+++ ch02/01_main_chapter_code/SimpleTokenizerV2.py
@@ -1,6 +1,6 @@
 import re
 
-class SimpleTokenizerV1:
+class SimpleTokenizerV2:
     """ 分词器""" 
     def __init__(self,vocab):
         self.str_to_int = vocab
@@ -9,9 +9,10 @@ class SimpleTokenizerV1:
     def encode(self,text):
         preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
         preprocessed = [
-           item.strip() for item in preprocessed if item.strip()
+           #去除空白字符
+           item.strip() for item in preprocessed if item.strip() #
         ]
-        # 使用<|unk|> 替换未在字典中的词元
+        # 使用<|unk|> 替换没在在字典中的词元
         preprocessed = [ item if item in self.str_to_int else "<|unk|>" for item in preprocessed] 
 
         ids = [self.str_to_int[item] for item in preprocessed]
