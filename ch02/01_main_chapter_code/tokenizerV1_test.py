26be334892e4b66ece02716e8c582796e6b8adaa
luyuan <luyuan202509@gmail.com>
2025-10-04 16:06:54 +0800

    词元id版本2

--- ch02/01_main_chapter_code/tokenizerV1_test.py
+++ ch02/01_main_chapter_code/tokenizerV1_test.py
@@ -8,30 +8,29 @@ file_path = os.path.join(paren_path,'01_main_chapter_code')
 file = file_path + '/the-verdict.txt'
 with open(file,'r',encoding="utf-8") as f:
     raw_text = f.read()
-    print("Total number of character:",len(raw_text))
     #print(raw_text[:99])
     preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text) 
-    print("Total number of character:",len(preprocessed)) # 包括空白字符
     preprocessed = [item.strip() for item in preprocessed if item.strip()]
-    print("Total number of character:",len(preprocessed)) # 不包括空白字符
-
-    # 打印前30 
-    print(preprocessed[:30])
-
+    
     # 词元转换为词元 ID
-    all_words = sorted(set(preprocessed))
-    vocab_size = len(all_words)
-    print("词元ID大小:",vocab_size) # 1130
+    all_tokens = sorted(set(preprocessed))
+    all_tokens.extend(["<|endoftext|>","<|unk|>"])
+    vocab_size = len(all_tokens)
+    #print("词元ID大小:",vocab_size) # 1132
 
     print("============================================================================")
     # 创建词汇表字典 
-    vocab = {word:i for i,word in enumerate(all_words)}
+    vocab = {word:i for i,word in enumerate(all_tokens)}
+
+    #验证词典
+    for i,item in enumerate(list(vocab.items())[-5:]):
+        print(i,item)
     
     tokenizer = SimpleTokenizerV1(vocab)
     
     text = """"It's the last he painted, you know, Mrs. Gisburn said with pardonable pride."""
     ids = tokenizer.encode(text)
-    print(ids)
+   # print(ids)
 
    # text2 = "Hello, do you like tea?" # 不在数据集中，会报错
     #print(tokenizer.encode(text2))
