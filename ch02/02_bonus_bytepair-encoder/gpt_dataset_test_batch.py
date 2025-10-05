73fcd5951f2519651342421c26d0c403aa73c67f
luyuan <luyuan202509@gmail.com>
2025-10-05 17:35:02 +0800

    创建数据集-多批次

--- ch02/02_bonus_bytepair-encoder/gpt_dataset_test_batch.py
+++ ch02/02_bonus_bytepair-encoder/gpt_dataset_test_batch.py
@@ -2,25 +2,19 @@ import os
 from gpt_dataset import GPTDataset as gd
 parenpath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 file = os.path.join(parenpath,'the-verdict.txt')
-
-"""
-创建数据集-多批次
-"""
+"""创建数据集-单批次
+ """
 
 with open(file,'r',encoding="utf-8") as f:
     raw_text = f.read()
 
     dataloader = gd.create_dataloader_v1(raw_text,
-                                      batch_size=1,
+                                      batch_size=8,
                                       max_length=4,
-                                      stride = 1,
+                                      stride = 4,
                                       shuffle = False
                                       )
-    
     data_iter = iter(dataloader)
-    first_batch = next(data_iter)
-    print(first_batch)
-    second_batch = next(data_iter)
-    print(second_batch)
-    third_batch = next(data_iter)
-    print(third_batch)
+    inputs, targets = next(data_iter)
+    print("Inputs:\n",inputs)
+    print("Targets:\n",targets)   
