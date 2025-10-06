71d397ed36cc87e94e0b12ef11337e95f941f948
luyuan <luyuan202509@gmail.com>
2025-10-06 13:43:00 +0800

    每个词元ID都已被嵌入一个256维的向量中

--- ch02/02_bonus_bytepair-encoder/gpt-enbedding.py
+++ ch02/02_bonus_bytepair-encoder/gpt-enbedding.py
@@ -8,13 +8,8 @@ from RawText import RawText as rt
 rt = rt()
 raw_text = rt.read()
 
-
-vocab_size = 50257 #词汇表
-output_dim = 256 # 输出维度
-embedding_layer = torch.nn.Embedding(vocab_size,output_dim)
-
 # 应用到一个词元 ID 上,以获取嵌入向量 这里引用在第三个词元，对应第4行权重
-#print(embedding_layer(torch.tensor([1])))
+#print(token_embedding_layer(torch.tensor([1])))
 
 max_length = 4
 data_loader = gd.create_dataloader_v1(raw_text,
@@ -27,7 +22,14 @@ data_loader = gd.create_dataloader_v1(raw_text,
 data_iter = iter(data_loader)
 inputs,targets = next(data_iter)
 
-print("tokenids:\n ",inputs)
-print("inputs shape:\n ",inputs.shape)
+vocab_size = 50257 #词汇表
+output_dim = 256 # 输出维度
+token_embedding_layer = torch.nn.Embedding(vocab_size,output_dim)
 
+# 应用到一个词元 ID 上,以获取“嵌入向量” 这里引用在第三个词元，对应第4行权重
+#print(token_embedding_layer(torch.tensor([1])))
+
+#应用在全部词元id上，获取所有ID嵌入向量
+token_embeddings =  token_embedding_layer(inputs)
+print(token_embeddings.shape)
 
